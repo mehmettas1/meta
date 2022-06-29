713acb6d13c09903e881d7874e4cc11f2cd0425c
mehmet <tas349873@gmail.com>
2022-06-29 21:35:43 +0300

    update

--- solution/src/components/item/Item.js
+++ solution/src/components/item/Item.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import "./Item.css";
+
+const Item = ({ card }) => {
+  const { name, img, option } = card;
+  return (
+    <div className="card">
+      <img className="card-logo" src={img} alt={name} />
+      <h3 className="card-title">{name}</h3>
+    </div>
+  );
+};
+    
+export default Item;
